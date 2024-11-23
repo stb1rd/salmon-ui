5d1d5baf0f6f2465fc357b0e66443e98aa44d1c9
Anton Vorobev <afvorobev@gmail.com>
2024-11-23 14:08:01 +0300

    feat: добавлены запросы в бек

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import { Layout } from '@/components/Layout';
 import '@/styles/globals.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </QueryClientProvider>
   );
 }
--- src/pages/log.tsx
+++ src/pages/log.tsx
@@ -1,15 +1,31 @@
+import { useQuery } from '@tanstack/react-query';
 import Link from 'next/link';
 
 export default function Home() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['log'],
+    queryFn: async () => {
+      const response = await fetch('http://194.87.131.215:8080/trout/rest/selects_grading');
+      return await response.json();
+    },
+  });
+
+  console.log('data', data);
+
+  if (isLoading) {
+    <span className="loading loading-spinner"></span>;
+  }
+
+  if (!data || data.content.length === 0) {
+    return <p>Ничего не найдено</p>;
+  }
+
   return (
-    <div className="hero py-10">
-      <div className="hero-content text-center flex flex-col">
-        <h1 className="text-3xl font-bold">Раздел в разработке</h1>
-        <p className="py-3">Скоро желаемый функционал будет реализован</p>
-        <Link href="/" className="btn btn-primary">
-          На главную
-        </Link>
-      </div>
-    </div>
+    <>
+      <h2 className="text-3xl">Оценка показателей</h2>
+      {data.content.map((x, i) => (
+        <pre key={i}>{JSON.stringify(x, null, ' ')}</pre>
+      ))}
+    </>
   );
 }
