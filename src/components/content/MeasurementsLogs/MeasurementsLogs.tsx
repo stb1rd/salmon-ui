71f2d9729ceba28831c389d6c031d672aa9acacd
Anton Vorobev <afvorobev@gmail.com>
2024-11-24 14:04:25 +0300

    feat: добавлены цвета в журнал

--- src/components/content/MeasurementsLogs/MeasurementsLogs.tsx
+++ src/components/content/MeasurementsLogs/MeasurementsLogs.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { ApiMeasurementItem, getDateLabel } from './helpers';
+import { ApiMeasurementItem, DiapasonItem, getDateLabel } from './helpers';
 import { labelsByNamePathDict } from '@/constants';
 import { round } from '@/components/utils/round';
 import { MOCKED_GRADES } from './mocked-grades';
@@ -9,7 +9,7 @@ export const MeasurementsLogs = () => {
     queryKey: ['log'],
     queryFn: async () => {
       try {
-        throw new Error();
+        // throw new Error();
         const response = await fetch('http://194.87.131.215:8080/trout/rest/selects_grading?size=1000&genderIn=female');
         return await response.json();
       } catch (error) {
@@ -23,6 +23,16 @@ export const MeasurementsLogs = () => {
   const shouldRenderIsLoading = isLoading && isDataEmpty;
   const shouldRenderDataEmpty = !isLoading && isDataEmpty;
   const shouldRenderData = (!isLoading && !isDataEmpty) || (isLoading && !isDataEmpty);
+
+  const getTdBg = (pathName: string, value?: string) => {
+    const initialD = (data.diapasons as DiapasonItem[]).find((d) => d.fieldName === pathName);
+    if (initialD && Number(value) > initialD.redBound) {
+      return 'bg-red-200';
+    }
+    if (initialD && Number(value) > initialD.yellowBound) {
+      return 'bg-yellow-200';
+    }
+  };
 
   return (
     <>
@@ -38,8 +48,8 @@ export const MeasurementsLogs = () => {
           <table className="table table-pin-rows table-pin-cols">
             <thead>
               <tr>
-                <th>#</th>
-                <th className="whitespace-pre-wrap pr-0 pl-[28px]">Метка (идентификатор)</th>
+                <th className="pr-0 pl-[28px]">#</th>
+                <th className="whitespace-pre-wrap">Метка (идентификатор)</th>
                 <td className="whitespace-pre-wrap">Дата оценки</td>
                 <td className="whitespace-pre-wrap">{labelsByNamePathDict.get('weight')} г</td>
                 <td className="whitespace-pre-wrap">{labelsByNamePathDict.get('lengthBody')} см</td>
@@ -68,24 +78,77 @@ export const MeasurementsLogs = () => {
                   <th className="pl-[28px]">{i + 1}</th>
                   <th>{measureItem.tag}</th>
                   <td>{getDateLabel(measureItem.created)}</td>
-                  <td className="text-right">{measureItem.weight}</td>
-                  <td className="text-right">{measureItem.lengthBody}</td>
-                  <td className="text-right">{measureItem.heightBody}</td>
-                  <td className="text-right">{measureItem.thicknessOfBack}</td>
-                  <td className="text-right">{measureItem.weightOfEggs}</td>
-                  <td className="text-right">{measureItem.weightOfSingleEgg}</td>
-                  <td className="text-right">{round(Number(measureItem.fatnessFactor))}</td>
-                  <td className="text-right">{round(Number(measureItem.thickFactor))}</td>
-                  <td className="text-right">{round(Number(measureItem.runnabilityIndex))}</td>
-                  <td className="text-right">{round(Number(measureItem.headFactor))}</td>
-                  <td className="text-right">{round(Number(measureItem.caviarProportion))}</td>
-                  <td className="text-right">{measureItem.workingFertility || 0}</td>
-                  <td className="text-right">{measureItem.relativeFecundity || 0}</td>
-                  <td className="text-right">{measureItem.reproductiveIndex || 0}</td>
-                  <td className="text-right">{measureItem.percentOfDeathToThePeephole}</td>
-                  <td className="text-right">{measureItem.percentOfDeathAfterThePeephole}</td>
-                  <td className="text-right">{measureItem.percentOfEggFertilization}</td>
-                  <td className="text-right">{measureItem.percentOfEmbryoSurvival}</td>
+                  <td className={`text-right ${getTdBg('weight', measureItem.weight)}`}>
+                    {round(Number(measureItem.weight))}
+                  </td>
+                  <td className={`text-right ${getTdBg('lengthBody', measureItem.lengthBody)}`}>
+                    {round(Number(measureItem.lengthBody))}
+                  </td>
+                  <td className={`text-right ${getTdBg('heightBody', measureItem.heightBody)}`}>
+                    {round(Number(measureItem.heightBody))}
+                  </td>
+                  <td className={`text-right ${getTdBg('thicknessOfBack', measureItem.thicknessOfBack)}`}>
+                    {round(Number(measureItem.thicknessOfBack))}
+                  </td>
+                  <td className={`text-right ${getTdBg('weightOfEggs', measureItem.weightOfEggs)}`}>
+                    {round(Number(measureItem.weightOfEggs))}
+                  </td>
+                  <td className={`text-right ${getTdBg('weightOfSingleEgg', measureItem.weightOfSingleEgg)}`}>
+                    {round(Number(measureItem.weightOfSingleEgg))}
+                  </td>
+                  <td className={`text-right ${getTdBg('fatnessFactor', measureItem.fatnessFactor)}`}>
+                    {round(Number(measureItem.fatnessFactor))}
+                  </td>
+                  <td className={`text-right ${getTdBg('thickFactor', measureItem.thickFactor)}`}>
+                    {round(Number(measureItem.thickFactor))}
+                  </td>
+                  <td className={`text-right ${getTdBg('runnabilityIndex', measureItem.runnabilityIndex)}`}>
+                    {round(Number(measureItem.runnabilityIndex))}
+                  </td>
+                  <td className={`text-right ${getTdBg('headFactor', measureItem.headFactor)}`}>
+                    {round(Number(measureItem.headFactor))}
+                  </td>
+                  <td className={`text-right ${getTdBg('caviarProportion', measureItem.caviarProportion)}`}>
+                    {round(Number(measureItem.caviarProportion))}
+                  </td>
+                  <td className={`text-right ${getTdBg('workingFertility', measureItem.workingFertility)}`}>
+                    {round(Number(measureItem.workingFertility)) || 0}
+                  </td>
+                  <td className={`text-right ${getTdBg('relativeFecundity', measureItem.relativeFecundity)}`}>
+                    {round(Number(measureItem.relativeFecundity)) || 0}
+                  </td>
+                  <td className={`text-right ${getTdBg('reproductiveIndex', measureItem.reproductiveIndex)}`}>
+                    {round(Number(measureItem.reproductiveIndex)) || 0}
+                  </td>
+                  <td
+                    className={`text-right ${getTdBg(
+                      'percentOfDeathToThePeephole',
+                      measureItem.percentOfDeathToThePeephole
+                    )}`}
+                  >
+                    {round(Number(measureItem.percentOfDeathToThePeephole))}
+                  </td>
+                  <td
+                    className={`text-right ${getTdBg(
+                      'percentOfDeathAfterThePeephole',
+                      measureItem.percentOfDeathAfterThePeephole
+                    )}`}
+                  >
+                    {round(Number(measureItem.percentOfDeathAfterThePeephole))}
+                  </td>
+                  <td
+                    className={`text-right ${getTdBg(
+                      'percentOfEggFertilization',
+                      measureItem.percentOfEggFertilization
+                    )}`}
+                  >
+                    {round(Number(measureItem.percentOfEggFertilization))}
+                  </td>
+                  <td
+                    className={`text-right ${getTdBg('percentOfEmbryoSurvival', measureItem.percentOfEmbryoSurvival)}`}
+                  >
+                    {round(Number(measureItem.percentOfEmbryoSurvival))}
+                  </td>
                   <th className="pr-[28px]">{i + 1}</th>
                 </tr>
               ))}
